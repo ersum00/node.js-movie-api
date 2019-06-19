31b03afb61e700e2472aa228b85550143bccceff
Eren <e.unluleblebici@gmail.com>
2019-06-19 18:34:04 +0200

    api/movies [Get] edit

--- routes/movie.js
+++ routes/movie.js
@@ -2,8 +2,21 @@ const express = require('express');
 const router = express.Router();
 //Models
 const Movie = require('../models/Movie');
+
 router.get('/',(req,res) => {
-  const promise = Movie.find({});
+  const promise = Movie.aggregate([
+    {
+      $lookup: {
+        from: 'directors',
+        localField: 'director_id',
+        foreignField: '_id',
+        as: 'director'
+      }
+    },
+    {
+      $unwind: '$director'
+    }
+  ]);
   promise.then((data) => {
     res.json(data);
   }).catch((err) => {
