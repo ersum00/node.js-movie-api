3ea013e45e1cb7b8f500c9a003d56d7e99fcfe7f
Eren <e.unluleblebici@gmail.com>
2019-06-19 17:15:04 +0200

    Director post

---
+++ routes/director.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+//Models
+const Director = require('../models/Director');
+
+router.post('/', (req, res, next) => {
+const director = new Director(req.body);
+    const promise = director.save();
+    promise.then((director)=> {
+        res.json(director);
+    }).catch((err) => {
+        if(err){
+            res.json(err);
+        }
+    });
+
+});
+
+module.exports = router;
